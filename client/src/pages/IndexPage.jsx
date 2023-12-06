31513f141e6447d15aba4b08a53ea68bf46d48cb
Sunny Anand <rvrahulvermaonly1@gmail.com>
2023-12-06 11:32:01 +0530

    Register and Signup frontend

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const IndexPage = () => {
+  return (
+    <div>
+    index page here
+  </div>
+
+  )
+}
+
+export default IndexPage
